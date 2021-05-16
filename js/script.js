d5ea4b6984d34e9a3dd83da8da17c696b618c014
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2021-05-16 13:13:00 +0100

    Changed text, font and emoji separated css and javascript added meta tags

---
+++ js/script.js
@@ -0,0 +1,70 @@
+/* ========================================== Youtube API ========================================== */
+
+// https://developers.google.com/youtube/iframe_api_reference
+
+// loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+// This function creates an <iframe> (and YouTube player)
+var player;
+function onYouTubeIframeAPIReady() {
+player = new YT.Player('player', {
+    height: 'auto',
+    width: 'auto',
+    videoId: 'fEeCb-2vuzA',
+    playerVars: {
+        'playsinline': 1,
+        'controls': 0,
+        'loop': 1,
+        'modestbranding': 1,
+        'rel': 0,
+        'showinfo': 0,
+        // listType:'playlist',
+        // list: '<playlist id here>'
+    },
+    events: {
+    'onReady': onPlayerReady,
+    'onError': onPlayerError,
+    'onStateChange': onPlayerStateChange
+    }
+});
+}
+
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    document.getElementById('player').style.display = 'none';
+    player.setShuffle(true);
+}
+
+// The API will call this function when the video player has an error.
+function onPlayerError(event) {
+    window.open(player.getVideoUrl(), '_blank');
+    // console.log(event);
+    player.nextVideo();
+    player.pauseVideo();
+}
+
+// The API will call this function when the video player changes state.
+function onPlayerStateChange(event) {
+    switch(event.target.getPlayerState()) {
+        case 2:
+            document.getElementById('player').style.display = 'none';
+            break;
+        case 1:
+            document.getElementById('player').style.display = 'block';
+            break;
+        default:
+            // console.log(event.target.getPlayerState());
+    }
+}
+
+/* ========================================== Website Functionality ========================================== */
+
+function togglePlayer() {
+    player.playVideo();
+}
